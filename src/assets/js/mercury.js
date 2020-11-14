2618ccfa0c7fb5c89645725aaa48d045ac300d03
Tyler Sinks <tyler.sinks@gmail.com>
2020-11-13 19:21:52 -0500

    add func convert Eyears to Mercury, pass spec

--- src/assets/js/mercury.js
+++ src/assets/js/mercury.js
@@ -1,4 +1,9 @@
 export default function Mercury(earthYears) {
   this.earthYearsExpected = earthYears;
-  this.marsYearsExpected = 0;
+  this.mercuryYearsExpected = 0;
+}
+
+Mercury.prototype.convertFromEarth = function() {
+  this.mercuryYearsExpected = Math.round(this.earthYearsExpected * (1/.24));
+  return this.mercuryYearsExpected;
 }
