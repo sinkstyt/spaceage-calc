50dd6bd99a94160e4d76de52527beca0d4af0e20
Tyler Sinks <tyler.sinks@gmail.com>
2020-11-13 19:03:36 -0500

    add func body for earth to venus spec and pass

--- src/assets/js/venus.js
+++ src/assets/js/venus.js
@@ -2,3 +2,8 @@ export default function Venus(earthYears) {
   this.earthYearsExpected = earthYears;
   this.venusYearsExpected = 0;
 }
+
+Venus.prototype.convertFromEarth = function() {
+  this.venusYearsExpected = Math.round(this.earthYearsExpected * (1/.62));
+  return this.venusYearsExpected;
+}
