860fdbd8d8f4a3e8955f4bd8925854e848583aea
Tyler Sinks <tyler.sinks@gmail.com>
2020-11-13 19:37:30 -0500

    write converter for Jupiter, achieve pass

--- src/assets/js/jupiter.js
+++ src/assets/js/jupiter.js
@@ -2,3 +2,8 @@ export default function Jupiter(earthYears) {
   this.earthYearsExpected = earthYears;
   this.jupiterYearsExpected = 0;
 }
+
+Jupiter.prototype.convertFromEarth = function() {
+  this.jupiterYearsExpected = Math.round(this.earthYearsExpected * (1/11.86));
+  return this.jupiterYearsExpected;
+}
